ac8cee9c1a243a83eb198ceb65db45db826ea337
Kevin Chi <kevinchi118@gmail.com>
2018-05-28 19:48:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,18 @@
 
+<<<<<<< HEAD
+=======
+function getIt() {
+  $('p').on('click', function(e){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img') 
+}
+
+$(document).ready(function(){
+>>>>>>> 6861d93547dd1b3f88387b82a71a62f29235f436
 
 function getIt() {
   $('p').on('click', function() {
